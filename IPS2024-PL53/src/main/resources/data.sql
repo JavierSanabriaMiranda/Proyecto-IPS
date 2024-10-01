fdf4cb0fee49b011849a1f6299669f42b7f2ca58
uo293758 <uo293758@uniovi.es>
2024-10-01 18:40:01 +0200

    Adición de tablas y datos. Adición de tablas de empleados, horarios, equipos, jugadores y entrenadores al schema.sql + datos de empleados no deportivos a data.sql

--- IPS2024-PL53/src/main/resources/data.sql
+++ IPS2024-PL53/src/main/resources/data.sql
@@ -1,10 +1,23 @@
 --Datos para carga inicial de la base de datos
 
 --Para giis.demo.tkrun:
-delete from carreras;
-insert into carreras(id,inicio,fin,fecha,descr) values 
-	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada'),
-	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3'),
-	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2'),
-	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1'),
-	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion');
+--delete from carreras;
+--insert into carreras(id,inicio,fin,fecha,descr) values 
+--	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada'),
+--	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3'),
+--	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2'),
+--	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1'),
+--	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion');
+
+
+DELETE FROM EMPLEADO_NO_DEPORTIVO;
+INSERT INTO EMPLEADO_NO_DEPORTIVO (DNI, NOMBRE, APELLIDO, FECHA_NACIMIENTO, TELEFONO, POSICION, SALARIO_ANUAL) 
+VALUES
+('12345678A', 'Juan', 'Pérez', '1985-07-15', '600123456', 'gerente', 45000.00),
+('87654321B', 'Ana', 'García', '1990-11-25', '600654321', 'vendedor de entradas/abonos', 32000.00),
+('45678912C', 'Carlos', 'López', '1978-02-10', '600789123', 'gestor de instalaciones', 38000.00),
+('65432187D', 'María', 'Fernández', '1982-05-05', '600321654', 'encargados de tienda', 30000.00),
+('32165498E', 'Lucía', 'Martínez', '1995-08-30', '600654987', 'jardinería', 27000.00),
+('78945612F', 'Pablo', 'Sánchez', '1987-12-22', '600987654', 'cocina', 28000.00),
+('95175384G', 'Laura', 'Hernández', '1992-03-18', '600753951', 'empleados de tienda', 25000.00),
+('85236974H', 'Pedro', 'Gómez', '1989-09-09', '600369852', 'director de comunicaciones', 50000.00);
--- IPS2024-PL53/src/main/resources/schema.sql
+++ IPS2024-PL53/src/main/resources/schema.sql
@@ -2,6 +2,97 @@
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
-drop table Carreras;
-create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+--drop table Carreras;
+--create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
 
+
+-- ¡¡¡IMPORTANTE!!!
+-- Se han de hacer los drops a la inversa de los create para evitar 
+-- fallos SQL por borrado de tablas que estan referenciadas en otras tablas
+
+------- ZONA DE DROPS ------- 
+
+DROP TABLE ENTRENADOR;
+DROP TABLE JUGADOR;
+DROP TABLE EQUIPO;
+DROP TABLE EMPLEADO_DEPORTIVO;
+DROP TABLE TURNO;
+DROP TABLE HORARIO;
+DROP TABLE EMPLEADO_NO_DEPORTIVO;
+
+
+------- ZONA DE CREATES ------- 
+
+CREATE TABLE EMPLEADO_NO_DEPORTIVO (
+    DNI varchar(9),
+    NOMBRE varchar (15),
+    APELLIDO varchar(15),
+    FECHA_NACIMIENTO date,
+    TELEFONO varchar(9),
+    POSICION varchar(40),
+    SALARIO_ANUAL decimal(8,2),
+    CONSTRAINT PK_EMP_NO_DEP PRIMARY KEY (DNI),
+    CONSTRAINT CK_POSICION CHECK (POSICION IN ('gerente', 
+        'vendedor de entradas/abonos', 'encargados de tienda', 
+        'gestor de instalaciones', 'empleados de tienda', 'jardinería', 
+        'cocina', 'director de comunicaciones')),
+);
+
+CREATE TABLE HORARIO (
+    DNI varchar(9),
+    COD_HORARIO varchar(10),
+    CONSTRAINT PK_HORARIO PRIMARY KEY (COD_HORARIO, DNI),
+    CONSTRAINT FK_HORARIO FOREIGN KEY (DNI) 
+        REFERENCES EMPLEADO_NO_DEPORTIVO (DNI)
+);
+
+CREATE TABLE TURNO (
+    HORA_INICIO TIME,
+    HORA_FIN TIME,
+    DIA_SEMANA INT,
+    DNI varchar(9),
+    COD_HORARIO varchar(10),
+    CONSTRAINT CK_DIA_SEMANA CHECK(DIA_SEMANA BETWEEN 1 AND 7),
+    CONSTRAINT FK_TURNO FOREIGN KEY (DNI, COD_HORARIO) 
+        REFERENCES HORARIO (DNI, COD_HORARIO)
+);
+
+CREATE TABLE EMPLEADO_DEPORTIVO (
+    DNI varchar(9),
+    NOMBRE varchar (15),
+    APELLIDO varchar(15),
+    FECHA_NACIMIENTO date,
+    TELEFONO varchar(9),
+    POSICION varchar(40),
+    SALARIO_ANUAL decimal(8,2),
+    CONSTRAINT PK_EMP_DEP PRIMARY KEY (DNI),
+    CONSTRAINT CK_POSICION_DEP CHECK (POSICION IN ('jugador', 
+                'entrenador')),
+);
+
+CREATE TABLE EQUIPO (
+    ID_EQUIPO varchar(10),
+    DNI_PRIMER_ENTRENADOR varchar(9),
+    DNI_SEGUNDO_ENTRENADOR varchar(9),
+    CONSTRAINT PK_EQUIPO PRIMARY KEY (ID_EQUIPO)
+);
+
+CREATE TABLE JUGADOR (
+    DNI varchar(9),
+    ID_EQUIPO varchar(10),
+    CONSTRAINT PK_JUGADOR PRIMARY KEY (DNI),
+    CONSTRAINT FK_JUGADOR FOREIGN KEY (DNI) 
+        REFERENCES EMPLEADO_DEPORTIVO (DNI),
+    CONSTRAINT FK_JUGADOR_EQUIPO FOREIGN KEY (ID_EQUIPO)
+        REFERENCES EQUIPO (ID_EQUIPO)
+);
+
+CREATE TABLE ENTRENADOR (
+    DNI varchar(9),
+    ID_EQUIPO varchar(10),
+    CONSTRAINT PK_ENTRENADOR PRIMARY KEY (DNI),
+    CONSTRAINT FK_ENTRENADOR FOREIGN KEY (DNI) 
+        REFERENCES EMPLEADO_DEPORTIVO (DNI),
+    CONSTRAINT FK_ENTRENADOR_EQUIPO FOREIGN KEY (ID_EQUIPO)
+        REFERENCES EQUIPO (ID_EQUIPO)
+);
