e70194b844a857ee6df106e3a54c28ccf2be7510
uo293758 <uo293758@uniovi.es>
2024-10-07 20:46:59 +0200

    Bug Fix al Añadir Empleados Deportivos
    Anteriormente los empleados deportivos solo se añadían en la tabla
    Empleado_Deportivo pero no en las tablas concretas "Jugador" y
    "Entrenador"

--- IPS2024-PL53/src/main/java/backend/data/empleados/commands/AddEmpleadoDeportivo.java
+++ IPS2024-PL53/src/main/java/backend/data/empleados/commands/AddEmpleadoDeportivo.java
@@ -8,6 +8,13 @@ public class AddEmpleadoDeportivo {
 	private static final String QUERY = "INSERT INTO EMPLEADO_DEPORTIVO "
 			+ "(ID_EMPLEADO_DEP, DNI, NOMBRE, APELLIDO, FECHA_NACIMIENTO, TELEFONO, SALARIO_ANUAL) "
 			+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
+	
+	private static final String QUERY_JUGADOR = "INSERT INTO JUGADOR "
+			+ "(ID_JUGADOR, ID_EQUIPO) "
+			+ "VALUES (?, ?)";
+	private static final String QUERY_ENTRENADOR = "INSERT INTO ENTRENADOR "
+			+ "(ID_ENTRENADOR, ID_EQUIPO, POSICION_ENTRENADOR) "
+			+ "VALUES (?, ?, ?)";
 			
 
 	EmpleadoDTO dto;
@@ -19,5 +26,9 @@ public class AddEmpleadoDeportivo {
 	
 	public void execute() {
 		db.executeUpdate(QUERY, dto.id, dto.DNI, dto.nombre, dto.apellido, dto.fechaNac, dto.telefono, dto.salarioAnual);
+		if (dto.posicion.equals("jugador"))
+			db.executeUpdate(QUERY_JUGADOR, dto.id, null);
+		else if (dto.posicion.equals("entrenador"))
+			db.executeUpdate(QUERY_ENTRENADOR, dto.id, null, null);;
 	}
 }
