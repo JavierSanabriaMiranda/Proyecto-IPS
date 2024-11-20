b94c8060a6d9dedfafc3d977a9bb76a984cd6686
UO293758 <UO293758@EII-L04-04.aulasuo.uniovi.es>
2024-11-20 09:39:50 +0100

    Borrada Clase ConfiguradorDeMenuBase
    Se ha eliminado la clase ConfiguradorDeMenuBase pues su existencia hacia
    que todos los empleados tuvieran acceso desde sus cuentas a la función
    de compra de acciones, lo cual podía provocar problemas pues haría que
    el mismo id se registrase como accionista y como empleado, lo que haría
    que no funcionase correctamente el sistema de log in

--- IPS2024-PL53/src/main/java/frontend/menu/ConfiguradorDeMenuAccionista.java
+++ IPS2024-PL53/src/main/java/frontend/menu/ConfiguradorDeMenuAccionista.java
@@ -1,19 +1,29 @@
 package frontend.menu;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class ConfiguradorDeMenuAccionista extends ConfiguradorDeMenuBase {
+public class ConfiguradorDeMenuAccionista implements ConfiguradorDeMenu {
 
 	@Override
-	protected void configurarMenuParaUsuario(JFrame menu, AplicacionMain app) {
+	public void configurarMenu(JFrame menu, AplicacionMain app) {
+		JMenuBar menuBar = menu.getJMenuBar();
+		
+		// Menú "Accionistas"
+        JMenu accionistasMenu = new JMenu("Accionistas");
+        
+        menuBar.add(accionistasMenu);
+		
 		// Añade el portal de Accionistas
 		JMenuItem portalAccionistas = new JMenuItem("Portal de accionistas");
 		portalAccionistas.addActionListener(e -> {
 			menu.setVisible(false);
 			app.inicializarPortalAccionistas();
 		});
-		super.menuAccionistas.add(portalAccionistas);
+		
+        accionistasMenu.add(portalAccionistas);
 	}
 
 }
